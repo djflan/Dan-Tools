d490ff247bba42c291e6db6e48e80f6324557305
Dan Flanigan <djflan@gmail.com>
2022-03-27 17:39:20 -0500

    fixed a bad }

--- Azure-DevOps/CommitPushAll.ps1
+++ Azure-DevOps/CommitPushAll.ps1
@@ -15,13 +15,15 @@ foreach($repo in $repos)
     Invoke-Expression -Command "cd ""$repoPath""" 
     $branch = (Invoke-Expression -Command "git rev-parse --abbrev-ref HEAD")
 
-    if ($branch -eq "azure-pipelines") {
-            Write-Host "Commit to $azdoShared"
-            Invoke-Expression "git add ."
-            Invoke-Expression -Command "git commit -m ""Updated Azure Pipelines"""
-            Invoke-Expression -Command "git push"
-        else {
-            Write-Host "not on azure pipelines branch"
-        }
+    if ($branch -eq "azure-pipelines") 
+	{
+        Write-Host "Commit to $azdoShared"
+        Invoke-Expression "git add ."
+        Invoke-Expression -Command "git commit -m ""Updated Azure Pipelines"""
+        Invoke-Expression -Command "git push"
+	}		
+    else
+	{
+        Write-Host "not on azure pipelines branch"
     }
 }
