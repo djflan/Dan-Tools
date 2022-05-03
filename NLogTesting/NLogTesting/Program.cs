fec642cd1b44296cb749fef7a9bf0d2d8dec1ff7
Dan Flanigan <djflan@gmail.com>
2022-05-03 15:55:22 -0500

    Created test project for NLog

---
+++ NLogTesting/NLogTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogTesting
+{
+    internal class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private static void Main()
+        {
+            Logger.Info("testing");
+        }
+    }
+}
