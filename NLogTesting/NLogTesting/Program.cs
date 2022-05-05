9153a8732e1c7af5a931dc884b1dc32d9e7bbfe7
Dan Flanigan <djflan@gmail.com>
2022-05-05 08:42:04 -0500

    Logging now works

--- NLogTesting/NLogTesting/Program.cs
+++ NLogTesting/NLogTesting/Program.cs
@@ -9,13 +9,17 @@ namespace NLogTesting
 {
     internal class Program
     {
-        private const string LoggerName = "apiContentLog";
+        private const string LoggerName = "PatientAccessApiContent";
         private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(LoggerName);
 
         private static void Main()
         {
-            var x = NLog.LogManager.Configuration;
-            Logger.Log(new LogEventInfo(LogLevel.Debug, LoggerName, "test"));
+            Log("My name is Dan Flanigan.");
+        }
+
+        private static void Log(string message)
+        {
+            Logger.Log(new LogEventInfo(LogLevel.Debug, LoggerName, message));
         }
     }
 }
