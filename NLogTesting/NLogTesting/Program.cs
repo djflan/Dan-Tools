07bd357d2b009a61e120d3b7921924130b7d1da1
Dan Flanigan <djflan@gmail.com>
2022-05-12 11:19:08 -0500

    Created MockHttpRequest, now need to reproduce the logging issue

--- NLogTesting/NLogTesting/Program.cs
+++ NLogTesting/NLogTesting/Program.cs
@@ -1,4 +1,11 @@
-﻿using NLog;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Moq;
+using NLog;
 
 namespace NLogTesting
 {
@@ -7,14 +14,40 @@ namespace NLogTesting
         private const string LoggerName = "PatientAccessApiContent";
         private static readonly NLog.Logger Logger = NLog.LogManager.GetLogger(LoggerName);
 
-        private static void Main()
+        private static async Task Main()
         {
-            Log("Basic text logging");
+            var data = "data";
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
+
+            var httpContext = new DefaultHttpContext
+            {
+                Request =
+                {
+                    Body = stream,
+                    ContentLength = stream.Length
+                }
+            };
+
+            await LogAsync(httpContext.Request);
         }
 
-        private static void Log(string message)
+        private static async Task LogAsync(HttpRequest request)
         {
-            Logger.Log(new LogEventInfo(LogLevel.Debug, LoggerName, message));
+            var readBuffer = new byte[Convert.ToInt32(request.ContentLength.Value)];
+
+            await request.Body.ReadAsync(readBuffer, 0, readBuffer.Length);
+            var readBodyText = Encoding.UTF8.GetString(readBuffer);
+
+            Logger.Log(new LogEventInfo(LogLevel.Debug, LoggerName, readBodyText));
         }
+
+        /*
+         * request is HttpRequest
+         * 
+						var buffer = new byte[Convert.ToInt32(request.ContentLength.Value)];
+						await request.Body.ReadAsync(buffer, 0, buffer.Length);
+						body = Encoding.UTF8.GetString(buffer);
+         */
     }
 }
