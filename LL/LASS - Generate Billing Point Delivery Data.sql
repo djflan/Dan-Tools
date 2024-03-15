4e5b472ee0c559b80aa6b79d4047cfdc937e6a1a
Dan Flanigan <djflan@gmail.com>
2024-03-14 19:20:50 -0500

    working

--- LL/LASS - Generate Billing Point Delivery Data.sql
+++ LL/LASS - Generate Billing Point Delivery Data.sql
@@ -18,7 +18,7 @@ DECLARE @LassSalesTaxBatches TABLE
 )
 
 INSERT INTO @LassSalesTaxBatches
-(
+    (
     SalesTaxBatchId,
     InvoiceLineItemKey,
     SalesTaxBatchStatusKey,
@@ -28,38 +28,42 @@ INSERT INTO @LassSalesTaxBatches
     TaxCategory,
     UserAdded,
     DateAdded,
-    IsActive
-)
+    IsActive)
 VALUES
--- (NEWID(), 3111640, 1, 'NCOA', 17, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1),
--- (NEWID(), 3111641, 1, 'Postage', 20, 'FR020100', 'Postage', 'dflanigan', GETDATE(), 1),
--- (NEWID(), 3111637, 1, 'Additional Pages', 13, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
-(NEWID(), 3111643, 1, 'ReturnLogic', 29, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1),
--- (NEWID(), 3111636, 1, 'Lettershop', 31, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
--- (NEWID(), 3111638, 1, 'Inserts', 2, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
--- (NEWID(), 3111639, 1, 'Duplex Fee', 9, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
-(NEWID(), 3111642, 1, 'ReturnLogic', 32, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1)
+    (NEWID(), 3111640, 1, 'NCOA', 17, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111641, 1, 'Postage', 20, 'FR020100', 'Postage', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111637, 1, 'Additional Pages', 13, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111643, 1, 'ReturnLogic', 29, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111636, 1, 'Lettershop', 31, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111638, 1, 'Inserts', 2, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111639, 1, 'Duplex Fee', 9, 'P0000000', 'Print', 'dflanigan', GETDATE(), 1),
+    (NEWID(), 3111642, 1, 'ReturnLogic', 32, 'SD020900', 'Service', 'dflanigan', GETDATE(), 1)
 
-DECLARE @InvoiceLineItemKey BIGINT = 0
-DECLARE @TotalInvoiceLineItemRows INT =
-        (
-            SELECT COUNT(*)FROM @LassSalesTaxBatches
-        )
-DECLARE @CurrentInvoiceLineItemRow INT = 0
+DECLARE @TotalInvoiceLineItemRows INT = ( SELECT COUNT(*) FROM @LassSalesTaxBatches )
 
-WHILE @CurrentInvoiceLineItemRow < @TotalInvoiceLineItemRows
+DECLARE @CurrentInvoiceLineItemRow INT = 1
+
+WHILE @CurrentInvoiceLineItemRow <= @TotalInvoiceLineItemRows
 BEGIN
-    DECLARE @LineItemCalculatorModule NVARCHAR(128) =
-            (
-                SELECT li.LineItemCalculatorModule
-                FROM LASS_InvoiceLineItems (NOLOCK) lili
-                    INNER JOIN LASS_LineItems (NOLOCK) li
-                        ON lili.LineItemKey = li.LineItemKey
-                WHERE lili.InvoiceLineItemKey = @InvoiceLineItemKey --li.LineItemKey = @InvoiceLineItemKey)
-            )
+    -- Get the InvoiceLineItemKey for the current row
+    DECLARE @InvoiceLineItemKey BIGINT = (
+        SELECT InvoiceLineItemKey
+    FROM (
+        SELECT InvoiceLineItemKey, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum
+        FROM @LassSalesTaxBatches) AS T
+    WHERE RowNum = @CurrentInvoiceLineItemRow)
+
+    -- Get the LineItemCalculatorModule for the current InvoiceLineItemKey
+    DECLARE @LineItemCalculatorModule NVARCHAR(128) = (
+        SELECT li.LineItemCalculatorModule
+            FROM LASS_InvoiceLineItems (NOLOCK) lili
+        INNER JOIN LASS_LineItems (NOLOCK) li
+            ON lili.LineItemKey = li.LineItemKey
+    WHERE lili.InvoiceLineItemKey = @InvoiceLineItemKey)
 
     SELECT @LineItemCalculatorModule
 
+    -- Increment the counter
     SET @CurrentInvoiceLineItemRow = @CurrentInvoiceLineItemRow + 1
 END
 
